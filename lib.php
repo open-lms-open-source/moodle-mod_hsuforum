952243f075e18886ed2042142fcd7e18a0a127ca
mrcroy <chris@moodlerooms.com>
2014-05-20 09:41:32 -0400

    INT-5972:  Adding missing names fields required for the function fullname() to work

--- lib.php
+++ lib.php
@@ -1651,6 +1651,10 @@ function hsuforum_print_recent_activity($course, $viewfullnames, $timestart) {
         $postuser->id = $post->userid;
         $postuser->firstname = $post->firstname;
         $postuser->lastname = $post->lastname;
+        $postuser->firstnamephonetic = $post->firstnamephonetic;
+        $postuser->lastnamephonetic = $post->lastnamephonetic;
+        $postuser->middlename = $post->middlename;
+        $postuser->alternatename = $post->alternatename;
 
         $postuser = hsuforum_anonymize_user($postuser, (object) array(
             'id' => $post->forum,
@@ -6306,8 +6310,8 @@ function hsuforum_print_posts_threaded($course, &$cm, $forum, $discussion, $pare
                     echo "</div>\n";
                     continue;
                 }
-                $postuser     = new stdClass;
-                $postuser->id = $post->userid;
+                $postuser 	   = new stdClass;
+                $postuser->id  = $post->userid;
 
                 username_load_fields_from_object($postuser, $post);
 
