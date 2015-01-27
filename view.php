b978d6dc75ab0376ec53b8e47c7cd650adf89489
gthomas2 <gthomas@moodlerooms.com>
2015-01-27 15:15:12 +0000

    INT-7287: Don't get course module if we already have it

--- view.php
+++ view.php
@@ -39,7 +39,9 @@
         $forum = $DB->get_record('forum', array('id' => $f));
         $params['f'] = $forum->id;
     } else {
-        $cm = get_coursemodule_from_id('hsuforum', $id);
+        if (!$cm = get_coursemodule_from_id('hsuforum', $id)){
+            print_error('missingparameter');
+        }
         $forum = $DB->get_record('hsuforum', array('id' => $cm->instance));
         $params['id'] = $cm->id;
     }
@@ -54,7 +56,7 @@
 
     $course = $DB->get_record('course', array('id' => $forum->course));
 
-    if (!$cm = get_coursemodule_from_instance("hsuforum", $forum->id, $course->id)) {
+    if (!$cm && !$cm = get_coursemodule_from_instance("hsuforum", $forum->id, $course->id)) {
         print_error('missingparameter');
     }
 
