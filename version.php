fbf738aaa1a648af888d53c86cc4a9688e76b4d1
gthomas2 <gthomas@moodlerooms.com>
2015-02-25 17:58:28 +0000

    INT-7431: Remove cron times in version.php

--- version.php
+++ version.php
@@ -27,9 +27,8 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2014121900;       // The current module version (Date: YYYYMMDDXX)
+$plugin->version   = 2014121901;       // The current module version (Date: YYYYMMDDXX)
 $plugin->requires  = 2014051203;       // Requires this Moodle version
 $plugin->component = 'mod_hsuforum';   // Full name of the plugin (used for diagnostics)
 $plugin->release   = '2.7.0 (Build: 20141229)';
 $plugin->maturity  = MATURITY_STABLE;
-$plugin->cron      = 60;
