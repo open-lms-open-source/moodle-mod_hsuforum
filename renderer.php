8ee64c348392f99167130c27c5d785292b808748
Corey Wallis <corey.wallis@blackboard.com>
2015-07-23 09:53:37 +0930

    INT-7932: fix issues raised in code review

--- renderer.php
+++ renderer.php
@@ -1296,7 +1296,7 @@ HTML;
      * @return string
      */
     public function simple_edit_discussion($cm, $postid = 0, array $data = array()) {
-        global $DB, $USER, $OUTPUT, $COURSE;
+        global $DB, $USER, $OUTPUT;
 
         $context = context_module::instance($cm->id);
         $forum = hsuforum_get_cm_forum($cm);
@@ -1351,7 +1351,7 @@ HTML;
                     }
                 } else {
                     // Yes - User is restricted to posting in their own group.
-                    $usergroups = groups_get_user_groups($COURSE->id);
+                    $usergroups = groups_get_user_groups($cm->course);
 
                     foreach ($usergroups as $grouping => $groups) {
                         foreach ($groups as $groupid) {
