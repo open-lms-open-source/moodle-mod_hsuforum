58079b26e02c42aa06862fc113fd5d34b2968c16
Mark Nielsen <mark@moodlerooms.com>
2017-06-30 09:08:54 -0700

    INT-11942: Bump version and release

--- version.php
+++ version.php
@@ -27,8 +27,8 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2016121302;       // The current module version (Date: YYYYMMDDXX)
+$plugin->version   = 2017063000;       // The current module version (Date: YYYYMMDDXX)
 $plugin->requires  = 2016120500;       // Requires this Moodle version
 $plugin->component = 'mod_hsuforum';   // Full name of the plugin (used for diagnostics).
-$plugin->release   = '3.2.2';
+$plugin->release   = '3.2.3';
 $plugin->maturity  = MATURITY_STABLE;
