84e6de04593d87475cb8d5fba44f55b4ace7e583
David Scotson <dscotson@moodlerooms.com>
2017-11-10 16:58:57 +0000

    INT-12423: update Adv Forum with 3.3.2 forum changes

--- view.php
+++ view.php
@@ -97,13 +97,15 @@
 
     echo ('<div id="discussionsview">');
 
-/// Some capability checks.
+    // Some capability checks.
+    $courselink = new moodle_url('/course/view.php', ['id' => $cm->course]);
+
     if (empty($cm->visible) and !has_capability('moodle/course:viewhiddenactivities', $context)) {
-        notice(get_string("activityiscurrentlyhidden"));
+        notice(get_string("activityiscurrentlyhidden"), $courselink);
     }
 
     if (!has_capability('mod/hsuforum:viewdiscussion', $context)) {
-        notice(get_string('noviewdiscussionspermission', 'hsuforum'));
+        notice(get_string('noviewdiscussionspermission', 'hsuforum'), $courselink);
     }
 
     echo $discussionview;
