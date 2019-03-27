201c6cb4c5ecd256425999622cc13094915b2450
Guillermo Alvarez <guillermo.alvarez@blackboard.com>
2019-03-27 10:30:21 -0500

    INT-14271: Bump plugin versions for 3.6.2

--- version.php
+++ version.php
@@ -28,7 +28,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->version   = 2018080801;       // The current module version (Date: YYYYMMDDXX)
-$plugin->requires  = 2018051700;       // Requires this Moodle version
+$plugin->requires  = 2018120300;       // Requires this Moodle version
 $plugin->component = 'mod_hsuforum';   // Full name of the plugin (used for diagnostics).
-$plugin->release   = '3.5.2';
+$plugin->release   = '3.6.3';
 $plugin->maturity  = MATURITY_STABLE;
