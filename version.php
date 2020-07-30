47931496a409bf1d1c843d0bee8a78e41ff7cf02
Mikhail Golenkov <mikhailgolenkov@catalyst-au.net>
2020-07-30 12:45:52 +1000

    Set correct default value for showrecent column.

--- version.php
+++ version.php
@@ -27,7 +27,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2021060100;       // The current module version (Date: YYYYMMDDXX)
+$plugin->version   = 2021061100;       // The current module version (Date: YYYYMMDDXX)
 $plugin->requires  = 2020110900;       // Requires this Moodle version
 $plugin->component = 'mod_hsuforum';   // Full name of the plugin (used for diagnostics).
 $plugin->release   = '3.10.3';
