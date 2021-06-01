59910a601ae989e46e76b8c257a4814c390dc44f
Fabian Batioja <fabian.batioja@openlms.net>
2021-06-01 22:14:04 +0000

    INT-17116: Bump versions for 3.10

--- version.php
+++ version.php
@@ -27,8 +27,8 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2020061100;       // The current module version (Date: YYYYMMDDXX)
-$plugin->requires  = 2020061500;       // Requires this Moodle version
+$plugin->version   = 2021060100;       // The current module version (Date: YYYYMMDDXX)
+$plugin->requires  = 2020110900;       // Requires this Moodle version
 $plugin->component = 'mod_hsuforum';   // Full name of the plugin (used for diagnostics).
-$plugin->release   = '3.9.3';
+$plugin->release   = '3.10.3';
 $plugin->maturity  = MATURITY_STABLE;
