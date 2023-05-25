deb08e999e91aeb51eb6b387bda85e2f984a876b
Matthias Opitz <m.opitz@ucl.ac.uk>
2023-05-25 17:13:27 +0100

    removed unused $arialabeldiscussion variable to address tons of behat issues

--- renderer.php
+++ renderer.php
@@ -639,7 +639,7 @@ class mod_hsuforum_renderer extends plugin_renderer_base {
 HTML;
 
         return <<<HTML
-<article id="p{$d->postid}" class="hsuforum-thread hsuforum-post-target clearfix" role="article" aria-label="$arialabeldiscussion"
+<article id="p{$d->postid}" class="hsuforum-thread hsuforum-post-target clearfix" role="article" aria-label=""
     data-discussionid="$d->id" data-postid="$d->postid" data-author="$author" data-isdiscussion="true" $attrs>
     <header id="h{$d->postid}" class="clearfix $unreadclass">
         <div class="hsuforum-thread-author">
