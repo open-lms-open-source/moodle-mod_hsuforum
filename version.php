a1350c0ad0149acd342fd757cd7a4eb180239f47
Bryan Cruz <bryan.cruz@openlms.net>
2023-10-04 12:04:09 -0500

    INT-19185: 4.1 MP2 Version bump

--- version.php
+++ version.php
@@ -27,7 +27,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2023073100;       // The current module version (Date: YYYYMMDDXX)
+$plugin->version   = 2023100400;       // The current module version (Date: YYYYMMDDXX)
 $plugin->requires  = 2022112800;       // Requires this Moodle version
 $plugin->component = 'mod_hsuforum';   // Full name of the plugin (used for diagnostics).
 $plugin->release   = '4.1.3';
