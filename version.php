732ca7eb11d62e8d6dca752737b80265de8c1e2f
juanfelipe.orozcoescobar <juanfelipe.orozcoescobar@openlms.net>
2024-04-02 19:17:33 -0500

    INT-19601: 4.1 MP5 Version Bump of Open LMS plugins

--- version.php
+++ version.php
@@ -27,7 +27,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2024012900;       // The current module version (Date: YYYYMMDDXX)
+$plugin->version   = 2024040200;       // The current module version (Date: YYYYMMDDXX)
 $plugin->requires  = 2022112800;       // Requires this Moodle version
 $plugin->component = 'mod_hsuforum';   // Full name of the plugin (used for diagnostics).
 $plugin->release   = '4.1.3';
